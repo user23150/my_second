93b1ada34302c2d0e17d9eb219729fe7913a0593
user23150 <2025639608@qq.com>
2026-03-31 16:27:07 +0800

    上传dayup365代码

---
+++ dayup365.py
@@ -0,0 +1,20 @@
+#dayup365.py
+def f(dayfactor):
+    end=1
+    for i in range(365):
+        if i %7 in [0,6]:
+            end*=(1-0.01)
+        else:
+            end*=(1+dayfactor)
+    return end
+dayfactor=0.001
+def fn(factor):
+    end=1
+    for i in range(365):
+        end*=(1+factor)
+    return end
+while f(dayfactor)<fn(0.01):
+    dayfactor+=0.001
+print(f(dayfactor))
+print(dayfactor)
+print(fn(0.01))
